034db0dec10a0984afa39ef181fa5fae93c86bd7
Ismael Mammadzada <mamedzade030@gmail.com>
2022-03-22 15:08:43 +0400

    Login, registration, update operations implemented

---
+++ dashboard/index.php
@@ -0,0 +1,63 @@
+<?php
+require_once "dash_header.php";
+require_once "dash_sidebar.php";
+
+$getUserInfo = $pdo->prepare("SELECT * FROM users WHERE id=:id");
+$getUserInfo->execute([':id'=>$_SESSION['id']]);
+$user = $getUserInfo->fetch(PDO::FETCH_ASSOC);
+
+
+?>
+	<!-- Dashboard Content
+	================================================== -->
+	<div class="dashboard-content-container" data-simplebar>
+		<div class="dashboard-content-inner" >
+			
+			<!-- Dashboard Headline -->
+			<div class="dashboard-headline">
+				<h3>Salam, <?php echo $user['first_name'];?>!</h3>
+				<span>Sizi yenidən görməyə şadıq</span>
+
+
+			</div>
+	
+			<!-- Fun Facts Container -->
+			<div class="fun-facts-container">
+				<div class="fun-fact" data-fun-fact-color="#36bd78">
+					<div class="fun-fact-text">
+						<span>Bitmiş əməliyyatlar</span>
+						<h4>0</h4>
+					</div>
+					<div class="fun-fact-icon"><i class="icon-material-outline-gavel"></i></div>
+				</div>
+				<?php if($user['user_type'] == 1){?>
+					<div class="fun-fact" data-fun-fact-color="#b81b7f">
+						<div class="fun-fact-text">
+							<span>Təqdim olunan xidmətlər</span>
+							<h4>0</h4>
+						</div>
+						<div class="fun-fact-icon"><i class="icon-material-outline-business-center"></i></div>
+					</div>
+				<?php } ?>
+				<div class="fun-fact" data-fun-fact-color="#efa80f">
+					<div class="fun-fact-text">
+						<span>Dəyərləndirmələr</span>
+						<h4>0</h4>
+					</div>
+					<div class="fun-fact-icon"><i class="icon-material-outline-rate-review"></i></div>
+				</div>
+
+				<!-- Last one has to be hidden below 1600px, sorry :( -->
+				<div class="fun-fact" data-fun-fact-color="#2a41e6">
+					<div class="fun-fact-text">
+						<span>This Month Views</span>
+						<h4>987</h4>
+					</div>
+					<div class="fun-fact-icon"><i class="icon-feather-trending-up"></i></div>
+				</div>
+			</div>
+			
+
+
+
+<?php include "dash_footer.php"; ?>
---
+++ index.php
@@ -0,0 +1,83 @@
+<?php
+include "header.php";
+
+?>
+<!-- Intro Banner
+================================================== -->
+<!-- add class "disable-gradient" to enable consistent background overlay -->
+<div class="intro-banner" data-background-image="assets/images/home-background.jpg">
+	<div class="container">
+
+		<!-- Intro Headline -->
+		<div class="row">
+			<div class="col-md-12">
+				<div class="banner-headline">
+					<h3>
+						<strong>Frilanserlərin xidmətlərini axtarın və sifariş edin.</strong>
+						<br>
+						<span>Minlərlə işəgötürən <strong class="color">Workify</strong> vasitəsilə frilanser tapıb.</span>
+					</h3>
+				</div>
+			</div>
+		</div>
+
+		<!-- Search Bar -->
+		<div class="row">
+			<div class="col-md-12">
+				<form action="dashboard/jobs_main.php" method="GET">
+					<div class="intro-banner-search-form margin-top-95">
+
+							<!-- Search Field -->
+							<div class="intro-search-field with-autocomplete">
+								<label class="field-title ripple-effect">Harada?</label>
+								<div class="input-with-icon" >
+									<select class="selectpicker" multiple data-size="7" title="Bütün lokasiyalar">
+										<option value="">Baku</option>
+									</select>
+								</div>
+							</div>
+
+							<!-- Search Field -->
+							<div class="intro-search-field">
+								<label for ="intro-keywords" class="field-title ripple-effect">Açar söz</label>
+								<input id="intro-keywords" type="text" name="keyword" placeholder="Açar söz">
+							</div>
+
+							<!-- Button -->
+							<div class="intro-search-button">
+								<button class="button ripple-effect">Axtar</button>
+							</div>
+					</div>
+				</form>
+			</div>
+		</div>
+
+		<!-- Stats -->
+		<div class="row">
+			<div class="col-md-12">
+				<ul class="intro-stats margin-top-45 hide-under-992px">
+					<li>
+						<strong class="counter">0</strong>
+						<span>Xidmət</span>
+					</li>
+					<li>
+						<strong class="counter">0</strong>
+						<span>Əməliyyat</span>
+					</li>
+					<li>
+						<strong class="counter">0</strong>
+						<span>Frilanser</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+
+	</div>
+</div>
+
+
+<!-- Content
+================================================== -->
+
+
+<?php include "footer.php"; ?>
