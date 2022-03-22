eed8202ba22b6b845cb59b567bc0d84091f2f364
Ismael Mammadzada <mamedzade030@gmail.com>
2022-03-22 18:08:36 +0400

    Heroku Mysql setup implemented

--- system/dbController.php
+++ system/dbController.php
@@ -1,11 +1,18 @@
 <?php
-$dsn = 'mysql:dbname=admin_freelancer;host=localhost;charset=utf8';
+
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+
+$dsn = 'mysql:dbname='.$cleardb_db.';host='.$cleardb_server.';charset=utf8';
 $user = 'root';
 $password = '';
 
 try
 {
-	$pdo = new PDO($dsn,$user,$password);
+	$pdo = new PDO($dsn,$cleardb_username,$cleardb_password);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e)
